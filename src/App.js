adbb56e9b5d59e1cb7505187aa5154c07ce4f1ae
Enrique Juan de Dios <juandedioz@gmail.com>
2017-04-07 23:02:55 -0500

    Add Material Design Lite to Project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let CSSlayout = "demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-header";
+let CSSheader = "mdl-layout__header mdl-color-text--white mdl-color--primary";
+let CSSheaderBox = "mdl-layout__header mdl-color-text--white mdl-color--primary";
+let CSSheaderTitle = "mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to the Zazk's Hell</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={CSSlayout}>
+          <header className={CSSheader}>
+            <div className={CSSheaderBox}>
+              <div className={CSSheaderTitle}>
+                <h3><i className="material-icons">chrome_reader_mode</i> Simple Sticky Notes</h3>
+              </div>
+            </div>
+          </header>
+
       </div>
+
     );
   }
 }
