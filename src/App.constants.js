7e250302b9e51d018221cc39ee5c7ddcfc8c5070
Enrique Juan de Dios <juandedioz@gmail.com>
2017-04-11 00:48:53 -0500

    Remove unused variables

---
+++ src/App.constants.js
@@ -0,0 +1,20 @@
+
+export const CSSlayout = "demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-header";
+export const CSSheader = "mdl-layout__header mdl-color-text--white mdl-color--primary";
+export const CSSheaderBox = "mdl-layout__header mdl-color-text--white mdl-color--primary";
+export const CSSheaderTitle = "mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tabexport const mdl-cell--12-col-desktop";
+export const CSSmainBox = "mdl-layout__content mdl-color--grey-100";
+export const CSSnotesBox = "mdl-cell mdl-cell--12-col mdl-cell--12-col-tabexport const mdl-grid";
+
+export const CSSnotesCell = "mdl-card mdl-shadow--2dp mdl-cell"; 
+export const CSSnotesTitle= "mdl-card__title mdl-color--blue-grey-200"; 
+export const CSSnotesCard = "mdl-card__title-text"; 
+export const CSSnotesBoxIcon = "mdl-card__supporting-text mdl-color-text--grey-600"; 
+export const CSSnotesBoxMessage = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label"; 
+export const CSSnotesBoxInput = "mdl-textfield__input"; 
+export const CSSnotesBoxLabel = "mdl-textfield__label" ; 
+export const CSSnotesButton = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--color"; 
+export const CSSnotesBoxTitle = "mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-cell--12-col-tabexport const mdl-cell--12-col-desktop"; 
+
+export const CSSsticky = 'mdl-cell--4-col-desktop mdl-card__supporting-text mdl-cell--12-col mdl-shadow--2dp mdl-cell--4-col-tabexport const mdl-card mdl-cell sticky-note';
+export const CSSstickyDelete = 'delete mdl-button mdl-js-button mdl-js-ripple-effect'; 
--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
-let CSSlayout = "demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-header";
-let CSSheader = "mdl-layout__header mdl-color-text--white mdl-color--primary";
-let CSSheaderBox = "mdl-layout__header mdl-color-text--white mdl-color--primary";
-let CSSheaderTitle = "mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop";
-let CSSmainBox = "mdl-layout__content mdl-color--grey-100";
-let CSSnotesBox = "mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid";
-
-let CSSnotesCell = "mdl-card mdl-shadow--2dp mdl-cell"; 
-let CSSnotesTitle= "mdl-card__title mdl-color--blue-grey-200"; 
-let CSSnotesCard = "mdl-card__title-text"; 
-let CSSnotesBoxIcon = "mdl-card__supporting-text mdl-color-text--grey-600"; 
-let CSSnotesBoxMessage = "mdl-textfield mdl-js-textfield mdl-textfield--floating-label"; 
-let CSSnotesBoxInput = "mdl-textfield__input"; 
-let CSSnotesBoxLabel = "mdl-textfield__label" ; 
-let CSSnotesButton = "mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--color"; 
-let CSSnotesBoxTitle = "mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop"; 
-
-let CSSsticky = 'mdl-cell--4-col-desktop mdl-card__supporting-text mdl-cell--12-col mdl-shadow--2dp mdl-cell--4-col-tablet mdl-card mdl-cell sticky-note';
+import * as Klass from './App.constants.js';
 
 class StickyNote extends Component{
 
-
-    componentDidMount (){
-      this.messageElement = this.refs.message;
-      this.dateElement = this.refs.date;
-      this.deleteButton = this.refs.delete;
-      this.deleteButton.addEventListener('click', () => this.deleteNote() );
+    componentDidMount (){ 
+      this.refs.delete.addEventListener('click', () => this.deleteNote() );
     }
-
 
     // Deletes the note by removing the element from the DOM and the data from localStorage.
     deleteNote() {
@@ -38,10 +15,10 @@ class StickyNote extends Component{
     }
     render(){
       return(
-        <div className={CSSsticky}>
+        <div className={Klass.CSSsticky}>
           <div className="message" ref="message">{this.props.message}</div>
           <div className="date" ref="date">{this.props.date}</div>
-          <button className="delete mdl-button mdl-js-button mdl-js-ripple-effect" ref="delete">
+          <button className={Klass.CSSstickyDelete} ref="delete">
               Delete
           </button>
         </div>
@@ -50,28 +27,18 @@ class StickyNote extends Component{
 }
 
 class App extends Component {
- 
     
     componentDidMount(){
-
-        // Shortcuts to DOM Elements.
-        this.notesContainer = document.getElementById('notes-container');
-        this.noteMessageInput = document.getElementById('message');
-        this.addNoteButton = document.getElementById('save');
-        this.notesSectionTitle = document.getElementById('notes-section-title');
-
         // Loads all the notes.
         for (let key in localStorage) {
             this.displayNote(key, localStorage[key]);
         }
-
         // Listen for updates to notes from other windows.
         window.addEventListener('storage', e=> this.displayNote(e.key, e.newValue) );
     }
 
     // Resets the given MaterialTextField.
     resetMaterialTextfield(element) {
-        console.log("Gay Reset");
         element.value = '';
         element.parentNode.MaterialTextfield.boundUpdateClassesHandler();
         element.blur(); 
@@ -90,7 +57,6 @@ class App extends Component {
         return `Created on  ${shortDate}`;
     }
  
-
     // Creates/updates/deletes a note in the UI.
     displayNote (key, message) {
         let note = document.getElementById(key); 
@@ -143,35 +109,35 @@ class App extends Component {
           notes.push( this.displayNote(key, localStorage[key]) );
       }
       return (
-        <div className={CSSlayout}>
-            <header className={CSSheader}>
-              <div className={CSSheaderBox}>
-                <div className={CSSheaderTitle}>
+        <div className={Klass.CSSlayout}>
+            <header className={Klass.CSSheader}>
+              <div className={Klass.CSSheaderBox}>
+                <div className={Klass.CSSheaderTitle}>
                   <h3><i className="material-icons">chrome_reader_mode</i> Simple Sticky Notes</h3>
                 </div>
               </div>
             </header> 
-            <main className={CSSmainBox}>
-              <div id="notes-container" className={CSSnotesBox}>
+            <main className={Klass.CSSmainBox}>
+              <div id="notes-container" className={Klass.CSSnotesBox}>
                 
-                <div className={CSSnotesCell}>
-                  <div className={CSSnotesTitle}>
-                    <h2 className={CSSnotesCard}>Add new note</h2>
+                <div className={Klass.CSSnotesCell}>
+                  <div className={Klass.CSSnotesTitle}>
+                    <h2 className={Klass.CSSnotesCard}>Add new note</h2>
                   </div>
-                  <div className={CSSnotesBoxIcon}>
-                      <div className={CSSnotesBoxMessage}>
-                        <textarea className={CSSnotesBoxInput} rows="3" id="message"  onKeyUp={this.toggleButton}></textarea>
-                        <label className={CSSnotesBoxLabel} htmlFor="message">Message text...</label>
+                  <div className={Klass.CSSnotesBoxIcon}>
+                      <div className={Klass.CSSnotesBoxMessage}>
+                        <textarea className={Klass.CSSnotesBoxInput} rows="3" id="message"  onKeyUp={this.toggleButton}></textarea>
+                        <label className={Klass.CSSnotesBoxLabel} htmlFor="message">Message text...</label>
                       </div>
-                      <a id="save" onClick={this.saveNote.bind(this)} disabled className={CSSnotesButton} > 
+                      <a id="save" onClick={this.saveNote.bind(this)} disabled className={Klass.CSSnotesButton} > 
                         Add
                       </a>
                   </div>
                 </div>  
 
-                <div id="notes-section-title" className={CSSnotesBoxTitle} >
-                  <div className={CSSnotesTitle}>
-                    <h2 className={CSSnotesCard}>Your sticky notes</h2>
+                <div id="notes-section-title" className={Klass.CSSnotesBoxTitle} >
+                  <div className={Klass.CSSnotesTitle}>
+                    <h2 className={Klass.CSSnotesCard}>Your sticky notes</h2>
                   </div>
                   {notes}
                 </div>
